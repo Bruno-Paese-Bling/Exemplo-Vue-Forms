d4eb277e4c00278dbcc4616b31ed5f7cc60d6904
Bruno Paese - Bling <bruno.paese@bling.com.br>
2024-09-05 08:45:04 -0300

    commit inicial

---
+++ src/views/FormView.vue
@@ -0,0 +1,24 @@
+<template>
+  <Panel header="Formulário de produto">
+    <h1>Produto</h1>
+    <InputText v-model="formStore.textInput"></InputText>
+  </Panel>
+</template>
+
+<script setup lang="js">
+import Panel from 'primevue/panel'
+import InputText from 'primevue/inputtext'
+import { useFormStore } from '@/stores/FormStore'
+
+const formStore = useFormStore()
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
